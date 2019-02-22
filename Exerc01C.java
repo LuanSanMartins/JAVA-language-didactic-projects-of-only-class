6c857034638e44308a3ecf859c5f2cde1957e9db
Luan Carlos Martins dos Santos <47756761+LuanSanMartins@users.noreply.github.com>
2019-02-22 00:28:17 -0300

    Prints the largest, and the smallest value.
    This is algorithm receive a set of five numbers, and prints the largest, and smallest number. Also, this algorithm print the average between the numbers largest and smallest.

---
+++ Exerc01C.java
@@ -0,0 +1,44 @@
+﻿package exerc01;
+
+import java.util.Scanner;
+
+public class Exerc01C {
+
+    public static void main(String[] args) {
+        
+        int[] vetor = new int[5];
+        int i;
+        float maiorValor = 0;
+        float menorValor = 999999999;
+        int position;
+        float media;
+        
+        Scanner s = new Scanner(System.in);
+        
+        for(i = 0; i < 5; i++){
+            
+            position = i + 1;
+            
+            System.out.print("Enter a " + position + "º number of sequence: ");
+            vetor[i] = s.nextInt();
+            
+            if(vetor[i] > maiorValor){
+                
+                maiorValor = vetor[i];
+                
+            } else if(vetor[i] < menorValor){
+                
+                menorValor = vetor[i];
+                
+            }
+            
+        }
+        
+        media = (menorValor + maiorValor)/2;
+        
+        System.out.println("The smaller value is: " + menorValor + " and the bigger value is: " + maiorValor);
+        System.out.println("The media of values are: " + media);
+        
+    }
+    
+}
