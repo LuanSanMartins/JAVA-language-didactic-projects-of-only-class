8298cf990cc6bf284b28ea692977c93001227618
Luan Carlos Martins dos Santos <47756761+LuanSanMartins@users.noreply.github.com>
2019-02-22 01:51:13 -0300

    Prints the largest and the smallest numbers.
    This is algorithm, in the same way a the other, prints the largest and the smallest number of set values too, but the other algorithm use the function "for", and this algorithm use the function "while".

---
+++ Exerc02C.java
@@ -0,0 +1,52 @@
+﻿package exerc02;
+
+import java.util.Scanner;
+
+public class Exerc02C {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        int[] vetor = new int[99999999];
+        int number;
+        int menorValor = 999999999;
+        int maiorValor = 0;
+        int i = 1;
+        int position;
+        int media;
+        
+        System.out.print("Enter with the 1º number of sequence: ");
+        number = s.nextInt();
+        
+        while(number != 0){
+            
+            position = i + 1;
+            
+            System.out.print("Enter with the " + position +"º number of sequence: ");
+            number = s.nextInt();
+            
+            vetor[i] = number;
+            
+            if(vetor[i] > maiorValor){
+                
+                maiorValor = vetor[i];
+                
+            } else if(vetor[i] < menorValor && vetor[i] != 0){
+            
+                menorValor = vetor[i];
+            
+            }
+            
+            i++;
+            
+        }
+        
+        media = (menorValor+maiorValor)/2;
+        
+        System.out.println("The smaller value is " + menorValor + " and the bigger value is " + maiorValor);
+        System.out.println("The media of values are: " + media);
+        
+    }
+    
+}
